23e958b6ee64aadab44481eae999d03704c6930a
sagarchandresha <sagar.bsquaresoft@gmail.com>
2022-07-14 15:51:58 +0530

    connect store and fetch store data through graphql query

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -2,8 +2,8 @@ import {defineConfig} from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    storeDomain: 'hydrogen-preview.myshopify.com',
-    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+    storeDomain: 'hydrogen-teststore.myshopify.com',
+    storefrontToken: '352bc22272ae85f65e79344b68b42b61',
     storefrontApiVersion: '2022-07',
   },
 });
